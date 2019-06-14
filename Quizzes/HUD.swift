bbea2f766c84daec3368800b79bc9e694b9b9993
yaming <yl4502@nyu.edu>
2019-06-14 15:25:05 -0400

    Add a head-up display file (HUD) which is still empty now.

---
+++ Quizzes/HUD.swift
@@ -0,0 +1,14 @@
+//
+//  HUD.swift
+//  Quizzes
+//
+//  Created by 林雅明 on 6/14/19.
+//  Copyright © 2019 林雅明. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class HUD: SKNode {
+    
+}
