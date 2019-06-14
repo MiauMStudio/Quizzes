c6ccf755e203d1a19eb0601b82095cbd0a320c7f
yaming <yl4502@nyu.edu>
2019-06-14 16:45:21 -0400

    fix the wrong of quiz 2

--- Quizzes/Levels/Level2.swift
+++ Quizzes/Levels/Level2.swift
@@ -26,7 +26,7 @@ class Level2: Level {
         let answer2_1 = "7"
         let answer2_2 = "8"
         let answer2_3 = "10"
-        let quiz2 = Question(question: question2, answers: [answer2_1, answer2_2, answer2_3], rightAnswer: answer2_2)
+        let quiz2 = Question(question: question2, answers: [answer2_1, answer2_2, answer2_3], rightAnswer: answer2_3)
         
         questions.append(quiz2)
     }
